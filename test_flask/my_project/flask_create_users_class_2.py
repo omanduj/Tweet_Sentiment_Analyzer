923230725fed43c5283aadbfa9fdd761fd45c511
omanduj <omanduj@gmail.com>
2021-08-18 20:25:03 -0500

    Fixes Get Requests

--- test_flask/my_project/flask_create_users_class_2.py
+++ test_flask/my_project/flask_create_users_class_2.py
@@ -2,14 +2,15 @@ from flask import Flask, url_for, render_template, request, jsonify
 from markupsafe import escape, Markup
 import yaml
 from test_flask.my_project.pythonAPY_ import main_API_one, create_twitter_url, main_API_two
+from test_flask.my_project.database import main_DB, select_info
 
 
 
 app = Flask(__name__)
 
-@app.route("/tweetanalysis/<string:user_name>", methods = ["POST"])
+@app.route("/tweetanalysis/<string:username>", methods = ["POST"])
 #CURL STUFF
-def main(user_name):
+def main(username):
     some_json = request.get_json()
     username = some_json["username"]
     count = some_json["num_of_tweets"]
@@ -18,6 +19,7 @@ def main(user_name):
     neg_emotion = emotions["neg"]
     neu_emotion = emotions["neu"]
     compound_emo = emotions["compound"]
+    main_DB(username, count, pos_emotion, neg_emotion, neu_emotion, compound_emo)
     #CURL LOGIC
     return(
     jsonify({
@@ -27,50 +29,51 @@ def main(user_name):
         "total_sentiment" : compound_emo,
         }))
 
-# @app.route("/tweetanalysis/<string:user_name>", methods = ["GET"])
-# def main2(user_name):
-#     emotions = main_API_one(user_name)
-#     pos_emotion = emotions["pos"]
-#     neg_emotion = emotions["neg"]
-#     neu_emotion = emotions["neu"]
-#     compound_emo = emotions["compound"]
-#     return(
-#     jsonify({
-#         "negative_sentiment" : neg_emotion,
-#         "positive_sentiment" : pos_emotion,
-#         "neutral_sentiment" : neu_emotion,
-#         "total_sentiment" : compound_emo,
-#         }))
-
-
-@app.route("/liked_tweet_analysis/<string:user_name>", methods = ["POST"])
-def main3(user_name):
-    some_json = request.get_json()
-    username = some_json["username"]
-    count = some_json["num_of_tweets"]
-    emotions = main_API_two(username, count)
-    pos_emotion = emotions["pos"]
-    neg_emotion = emotions["neg"]
-    neu_emotion = emotions["neu"]
-    compound_emo = emotions["compound"]
-    #CURL LOGIC
+@app.route("/tweetanalysis/<string:username>", methods = ["GET"])
+def main2(username):
+    emotions = select_info()
+    username = emotions["username"]
+    pos_emotion = emotions["pos_emotion"]
+    neg_emotion = emotions["neg_emotion"]
+    neu_emotion = emotions["neu_emotion"]
+    compound_emo = emotions["total_sentiment"]
     return(
     jsonify({
+        "username": username,
         "negative_sentiment" : neg_emotion,
         "positive_sentiment" : pos_emotion,
         "neutral_sentiment" : neu_emotion,
         "total_sentiment" : compound_emo,
         }))
 
-# @app.route("/liked_tweet_analysis/<string:user_name>", methods = ["GET"])
-# def main4(user_name):
-#     emotions = main_API_two(user_name)
-#     pos_emotion = emotions["pos"]
-#     neg_emotion = emotions["neg"]
-#     neu_emotion = emotions["neu"]
-#     compound_emo = emotions["compound"]
+@app.route("/liked_tweet_analysis/<string:username>", methods = ["POST"])
+def main3(username):
+    emotions = select_info()
+    username = emotions["username"]
+    pos_emotion = emotions["pos_emotion"]
+    neg_emotion = emotions["neg_emotion"]
+    neu_emotion = emotions["neu_emotion"]
+    compound_emo = emotions["total_sentiment"]
+    return(
+    jsonify({
+        "username": username,
+        "negative_sentiment" : neg_emotion,
+        "positive_sentiment" : pos_emotion,
+        "neutral_sentiment" : neu_emotion,
+        "total_sentiment" : compound_emo,
+        }))
+
+# @app.route("/liked_tweet_analysis/<string:username>", methods = ["GET"])
+# def main4(username):
+#     emotions = select_info()
+#     username = emotions["username"]
+#     pos_emotion = emotions["pos_emotion"]
+#     neg_emotion = emotions["neg_emotion"]
+#     neu_emotion = emotions["neu_emotion"]
+#     compound_emo = emotions["total_sentiment"]
 #     return(
 #     jsonify({
+#         "username": username,
 #         "negative_sentiment" : neg_emotion,
 #         "positive_sentiment" : pos_emotion,
 #         "neutral_sentiment" : neu_emotion,
